0f2de9b028b0982559fc49d469fb52df8c502752
KWBezerra <kawanbezerra90@gmail.com>
2025-12-02 23:26:08 -0300

    Incluindo os arquivos, com o main organizado

---
+++ main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine, get_db
+import models
+
+#cria as tabelas
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Comfigurando a pasta de Templates.
+templates = Jinja2Templates(directory="templates")
+
+#-----------------Todos os GET-----------------#
+
+# 🔵 GET /articles/new-article → Chama a pagina do formulario de artigo
+@app.get("/articles/new-article")
+def create_article_page (request: Request):    
+    return templates.TemplateResponse("new-article.html", {"request": request})
+
+# 🔵 GET /home → Chama a pagina home e mandas para o template todos os artigos
+@app.get("/home")
+def list_articles(request: Request, db: Session = Depends(get_db)):
+    articles = db.query(models.Article).all()
+    return templates.TemplateResponse("home.html", {"request": request, "articles": articles})
+
+# 🔵 GET /admin -> Chama a pagina de Admin
+@app.get("/admin")
+def goto_admin(request: Request, db: Session = Depends(get_db)):
+    articles = db.query(models.Article).all()
+    return templates.TemplateResponse("admin.html", {"request": request, "articles": articles})
+
+## 🔵 GET /article/{id_article} -> Chama a pagina com o article completo
+@app.get("/article/{id_article}")
+def goto_article_complete(request: Request, id_article: int, db: Session=Depends(get_db)):
+    article = db.query(models.Article).filter(models.Article.id == id_article).first()
+    return templates.TemplateResponse("article.html", {"request": request, "article": article})
+
+## 🔵 GET /edit/{id_article} -> Chama a pagina para edição do artigo
+
+
+
+#-----------------Todos os POST-----------------#
+
+# 🟢 POST /articles -> Criar artigo
+@app.post("/articles")
+def create_article(title: str=Form(), content: str=Form(), db: Session = Depends(get_db)):
+    db_article = models.Article(title=title, content=content) 
+
+    db.add(db_article)
+    db.commit()    
+    
+    return RedirectResponse(url="/admin", status_code=303)
+
+
+# 🔴 POST /articles/{id} → deletar artigo
+@app.post("/delete/{id_article}")
+def delete_article(id_article: int, db: Session = Depends(get_db)):
+    db_article = db.query(models.Article).filter(models.Article.id == id_article).first()
+    if not db_article:
+        raise HTTPException(status_code=404, detail="Article not found")
+    db.delete(db_article)
+    db.commit()
+    return RedirectResponse(url="/admin", status_code=303)
+
+
+
+
+
+#-----------------Todos os PUT-----------------#
+
+# # 🟡 PUT /articles/{id} → atualizar artigo
+# @app.put("/articles/{id_article}")
+# def update_user(id_article: int, db : Session = Depends(get_db)):
+#     db_article = db.query(models.Article).filter(models.Article.id == id_article).first()
+#     if not db_article:
+#         raise HTTPException(status_code=404, detail="Article not found")
+    
+#     db_article.title   = article.title
+#     db_article.content = article.content
+#     db.commit()
+#     db.refresh(db_article)
+#     return db_article
+
---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+import datetime
+class Article(Base):
+    __tablename__ = "articles"
+    id         = Column(Integer, primary_key=True, index=True)
+    title      = Column(String(100), index=True)
+    content    = Column(String(1000))
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, default=None)
+
